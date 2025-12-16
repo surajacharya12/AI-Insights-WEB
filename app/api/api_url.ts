01eab79e04c4065d8799f0b2726b16d9b7732aa3
surajacharya12 <surajacharya.080@kathford.edu.np>
2025-12-16 18:20:25 +0545

    fix

--- app/api/api_url.ts
+++ app/api/api_url.ts
@@ -1,3 +1,6 @@
-const API_URL = "http://localhost:3001";
+const API_URL =
+    process.env.NODE_ENV === "production"
+        ? "https://ai-insights-backend-k84d.vercel.app"
+        : "http://localhost:3001";
 
 export default API_URL;
